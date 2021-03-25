8b524e9083dd0acda8856f71eedc43de99617bbf
Jay Mehta <31812582+thisisjaymehta@users.noreply.github.com>
2021-03-25 12:03:34 +0530

    Create files.py

---
+++ files.py
@@ -0,0 +1,10 @@
+files = [
+	'brux1', 'brux2',
+	'ins1', 'ins2', 'ins3', 'ins4', 'ins5', 'ins6', 'ins7', 'ins8', 'ins9',
+	'n1', 'n10', 'n11', 'n12', 'n13', 'n14', 'n15', 'n16', 'n2', 'n3', 'n4', 'n5', 'n6', 'n7', 'n8', 'n9',
+	'narco1', 'narco2', 'narco3', 'narco4', 'narco5',
+	'nfle1', 'nfle10', 'nfle11', 'nfle12', 'nfle13', 'nfle14', 'nfle15', 'nfle16', 'nfle17', 'nfle18', 'nfle19', 'nfle2', 'nfle20', 'nfle21', 'nfle22', 'nfle23', 'nfle24', 'nfle25', 'nfle26', 'nfle27', 'nfle28', 'nfle29', 'nfle3', 'nfle30', 'nfle31', 'nfle32', 'nfle33', 'nfle34', 'nfle35', 'nfle36', 'nfle37', 'nfle38', 'nfle39', 'nfle4', 'nfle40', 'nfle5', 'nfle6', 'nfle7', 'nfle8', 'nfle9',
+	'plm1', 'plm10', 'plm2', 'plm3', 'plm4', 'plm5', 'plm6', 'plm7', 'plm8', 'plm9',
+	'rbd1', 'rbd10', 'rbd11', 'rbd12', 'rbd13', 'rbd14', 'rbd15', 'rbd16', 'rbd17', 'rbd18', 'rbd19', 'rbd2', 'rbd20', 'rbd21', 'rbd22', 'rbd3', 'rbd4', 'rbd5', 'rbd6', 'rbd7', 'rbd8', 'rbd9',
+	'sdb1', 'sdb2', 'sdb3', 'sdb4'
+]
